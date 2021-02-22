42aa5a41bf7bac6d446be8613b645147ee61da2b
Ricardo Flores <rik.fm1809@gmail.com>
2021-02-22 15:54:01 -0500

    actualizar panel lateral y mensajes de guardado

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+    const dispatch = useDispatch();
     const { active:note } = useSelector(state => state.notes);
     const [ formValues, HandleInputChange, reset ] = useForm( note ); 
     const { body, title } = formValues;
@@ -16,6 +18,10 @@ export const NoteScreen = () => {
             activeId.current = note.id
         }
     }, [note, reset])
+
+    useEffect(() => {
+        dispatch( activeNote( formValues.id, { ...formValues } ) )
+    }, [formValues, dispatch])
 
     return (
         <div className="notes__main-content">
